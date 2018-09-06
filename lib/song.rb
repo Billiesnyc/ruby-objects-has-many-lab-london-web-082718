9ea0765406c30f9b56eedb7a42d750b525a82dca
Billie Simmons <billiesnyc@gmail.com>
2018-09-06 11:37:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,3 @@
 class Song 
   
-  attr_accessor :artist, :name
-    @@all = [ ]
-  
-
-  def initialize(name)
-    @name = name 
-    @@all << self
-  end 
-  
-  def self.all 
-    @@all 
-  end
-  
-  def artist_name
-    if self.artist 
-      self.artist.name
-    else
-      NIL
-    end
-    
-  end
-      
 end
